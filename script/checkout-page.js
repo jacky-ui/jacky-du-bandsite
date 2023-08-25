534268263ec9f7039ba596cf93b2d30096f903e7
jacky-ui <jacky.du.004@gmail.com>
2023-08-25 17:31:07 -0400

    grabbed and initialized values from checkout page. tried console log and works

---
+++ script/checkout-page.js
@@ -0,0 +1,6 @@
+// Grab and initialize values
+let cartSubheader = document.querySelector("#cartSubheader");
+let cartPara = document.querySelector("#cartPara");
+console.log(cartPara);
+
+let cart = 0;
