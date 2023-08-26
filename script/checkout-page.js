3a000fd011adf0ac77d528664e463d084a0b5d85
jacky-ui <jacky.du.004@gmail.com>
2023-08-25 22:15:55 -0400

    create two functions. One to remove class and the other to edit text

--- script/checkout-page.js
+++ script/checkout-page.js
@@ -1,15 +1,24 @@
 // Grab and initialize values
 let cartSubheader = document.querySelector("#cartSubheader");
 let cartPara = document.querySelector("#cartPara");
+let cartItemContainer = document.querySelector(".cart");
 
-// Grab variable from localStorage
-let customerCart = localStorage.cartItems;
+// Grab variable from sessionStorage
+let customerCart = sessionStorage.cartItems;
 console.log(customerCart);
+console.log(cartItemContainer);
 
 function editText(element, text) {
     element.innerText = text;
 };
 
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
 if(customerCart === 0) {
     editText(cartSubheader, "Your shopping cart is empty")
-} else if(customerCart > 0) editText(cartSubheader, "Your shopping cart");
+} else if(customerCart > 0) {
+    editText(cartSubheader, "Your shopping cart");
+    removeClass(cartItemContainer, "element--hidden");
+};
