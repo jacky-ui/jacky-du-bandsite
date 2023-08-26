a6038b563dbb0047b72c2acec559590bac626b4b
jacky-ui <jacky.du.004@gmail.com>
2023-08-25 22:35:03 -0400

    experience bug with trying to hide subtotal

--- script/checkout-page.js
+++ script/checkout-page.js
@@ -2,11 +2,13 @@
 let cartSubheader = document.querySelector("#cartSubheader");
 let cartPara = document.querySelector("#cartPara");
 let cartItemContainer = document.querySelector(".cart");
+let cartSubtotalText = document.querySelector(".cart__subtotal");
 
-// Grab variable from sessionStorage
-let customerCart = sessionStorage.cartItems;
+localStorage.setItem("cartItem", 2);
+
+// Grab variable from localStorage
+let customerCart = localStorage.cartItems;
 console.log(customerCart);
-console.log(cartItemContainer);
 
 function editText(element, text) {
     element.innerText = text;
@@ -14,11 +16,17 @@ function editText(element, text) {
 
 function removeClass(element, className) {
     element.classList.remove(className);
-}
+};
+
+function addClass(element, className) {
+    element.classList.add(className);
+};
 
 if(customerCart === 0) {
-    editText(cartSubheader, "Your shopping cart is empty")
+    editText(cartSubheader, "Your shopping cart is empty");
 } else if(customerCart > 0) {
     editText(cartSubheader, "Your shopping cart");
+    addClass(cartPara, "element--hidden");
     removeClass(cartItemContainer, "element--hidden");
+    removeClass(cartSubtotalText, "element--hidden");
 };
